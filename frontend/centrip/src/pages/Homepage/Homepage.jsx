85ff98a026195831861773d1c3892adddcd8ebff
Kev1nX <kevin0706x@gmail.com>
2022-02-27 13:31:58 +1100

    added styling for the homepage

--- frontend/centrip/src/pages/Homepage/Homepage.jsx
+++ frontend/centrip/src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { observer } from 'mobx-react';
 import MainContent from './MainContent/MainContent';
+import './HomepageCss.css';
 
 const Homepage = observer(({ store }) => {
+  const mystyle = {
+    width: "100vw",
+    height: "450px",
+    objectFit: "cover",
+  };
   return (
     <div>
-        <p>Homepage</p>
-        <MainContent store={store}/>
+      <div className='header'>
+        <h1>Centrip</h1>
+        <p>Saving you money wherever you go</p>
+      </div>
+
+        <img style= {mystyle}src ={require("../../ImageAssets/background.jpg")}></img>
+        <div className='Banner'>
+          <h2>Plan your trip</h2>
+          <p>We will calculate the cheapest and most efficient route and transport options tos save your money and time</p>
+        </div>
+
+
+        <div className = 'mainContentBox'>
+          <MainContent store={store}/>
+        </div>
+
+        
     </div>
   )
 });
